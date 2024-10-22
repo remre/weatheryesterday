967acbb99724be7683393e7a336d29d5cd9884df
remre <basarremre@outlook.com>
2024-10-22 07:06:22 +0300

    deneyek

--- src/components/HeaderBar/HeaderBar.tsx
+++ src/components/HeaderBar/HeaderBar.tsx
@@ -1,24 +1,34 @@
+// src/components/HeaderBar/HeaderBar.tsx
 'use client';
 import React, { useState } from 'react';
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
-
 import { useWeather } from '@/context/WeatherContext';
 import { useRouter } from 'next/navigation';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const HeaderBar = () => {
-  const { locationData } = useWeather();
+  const { locationData, fetchWeather, fetchPastWeather } = useWeather();
   const router = useRouter();
   const [city, setCity] = useState('');
-  const { fetchWeather } = useWeather();
+  const [date, setDate] = useState('');
 
   const handleCityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setCity(e.target.value);
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDate(e.target.value);
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    fetchWeather(city);
-    router.push('/today');
+    if (date) {
+      await fetchPastWeather(city, date);
+      router.push('/past');
+    } else {
+      await fetchWeather(city);
+      router.push('/today');
+    }
   };
 
   return (
@@ -30,6 +40,13 @@ const HeaderBar = () => {
           onChange={handleCityChange}
           placeholder="Enter city name"
           className="border p-2"
+        />
+        <input
+          type="date"
+          value={date}
+          onChange={handleDateChange}
+          placeholder="Enter date"
+          className="border p-2 ml-2"
         />
         <button type="submit" className="bg-white text-blue-500 p-2 ml-2">
           Search
