204716212b3127f57b5e0307bdf3e7e4ba9b1908
remre <basarremre@outlook.com>
2024-10-28 15:35:00 +0300

    feat; change font family

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 export default function NavBar() {
   return (
-    <div className="flex w-full bg-yellow-400 justify-between p-4">
+    <div className="flex w-full bg-yellow-400 justify-between p-4 text-xl">
       <Link href="/today">Today</Link>
       <Link href="/daily">Daily</Link>
       <Link href="/weekly">Weekly</Link>
